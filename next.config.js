df219ae4adf94f61ec0e528636dfe4d297601506
Joshua Cunningham <josh@joshcstudios.com>
2022-11-09 21:46:16 -0600

    Fixed env variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    DYNAMODB_ACCESS_KEY: process.env.DYNAMODB_ACCESS_KEY,
+    DYNAMODB_SECRET_KEY: process.env.DYNAMODB_SECRET_KEY,
+    DYNAMODB_REGION: process.env.DYNAMODB_REGION,
+    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME
+  }, 
   reactStrictMode: true,
   swcMinify: true,
 }
